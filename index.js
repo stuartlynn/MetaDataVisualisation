6f6c9eac13d31a3ab7642b9041b7fafd41666cf3
Stuart Lynn <stuart.lynn@gmail.com>
2020-02-20 15:16:58 -0500

    overlays

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import createREGL from 'regl';
 import tween from 'regl-tween';
 import circleVert from './circleVert.glsl';
 import circleFrag from './circleFrag.glsl';
+import colorKey from './color_key.json';
 
 //import data from './dataset_stats.csv';
 
@@ -11,28 +12,108 @@ import circleFrag from './circleFrag.glsl';
 
 console.log(circleVert, circleFrag);
 
-d3.csv(`${process.env.PUBLIC_URL}/dataset_stats.csv`).then(datasets => {
-  d3.csv(`${process.env.PUBLIC_URL}/links.csv`).then(links => {
-    console.log('datasets are ', datasets);
-    console.log('links are ', links[0]);
-    let nodes = datasets.map((a, i) => {
+let wx = 0;
+let wy = 0;
+
+var selectedID = null;
+
+function findNearest(x, y, nodes, width, height) {
+  const xx = x * width;
+  const yy = y * height;
+  console.log('xx ', xx, ' yy ', yy, ' x ', x, ' y ', y);
+  const hits = nodes.filter(
+    n => (n.x - xx) * (n.x - xx) + (n.y - yy) * (n.y - yy) < n.r * n.r,
+  );
+  return hits;
+}
+
+function setSelected(selected) {
+  const selectedDiv = document.getElementById('selected');
+  if (selected) {
+    console.log(selected);
+    const selectedTemplate = ` 
+      <h1>Selection</h1>
+      <p> name: ${selected.name} </p>
+      <p> agency: ${selected.agency} </p>
+      <p> downloads: ${selected.downloads} </p>
+      <p> views: ${selected.page_views} </p>
+    `;
+    selectedDiv.innerHTML = selectedTemplate;
+  } else {
+    selectedDiv.innerHTML = '';
+  }
+}
+
+const BASE_URL = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : '';
+
+d3.csv(`${BASE_URL}/dataset_stats.csv`).then(datasets => {
+  d3.csv(`${BASE_URL}/links.csv`).then(links => {
+    var nodes = datasets.map((a, i) => {
       const angle = Math.random() * 2.0 * Math.PI;
       return {
-        x: Math.cos(angle) * (700 + 500 * i), // (Math.random() - 0.5) * 20,
-        y: Math.sin(angle) * (700 + 500 * i),
+        ...a,
+        x: Math.cos(angle) * (window.innerWidth + 500 * i), // (Math.random() - 0.5) * 20,
+        y: Math.sin(angle) * (window.innerWidth + 500 * i),
         r: 10, //Math.random() * 10 + 1,
         color: [parseFloat(a.r), parseFloat(a.g), parseFloat(a.b), 1], //.0Math.random(), Math.random(), Math.random(), 1],
         id: i,
       };
     });
 
-    const linkForce = d3.forceLink(links);
+    document.addEventListener('mousemove', e => {
+      wx = (e.pageX / window.innerWidth - 0.5) * 2;
+      wy = -1.0 * (e.pageY / window.innerHeight - 0.5) * 2;
+      const near = findNearest(
+        wx,
+        wy,
+        nodes,
+        window.innerWidth,
+        window.innerHeight,
+      );
+      if (near.length > 0) {
+        console.log('near ', near[0].id);
+        selectedID = near[0].id;
+        setSelected(near[0]);
+      } else {
+        setSelected(null);
+        selectedID = null;
+      }
+    });
+
+    const setColorLegend = () => {
+      const keyDiv = document.getElementById('color-key');
+      if (showColor) {
+        const keyString = `
+            <h1>Departments</h1>
+            <div class='entries'>
+                ${Object.entries(colorKey)
+                  .map(
+                    a =>
+                      `<div class='entry'><div class='circle' style='background-color:rgb(${a[1][0] *
+                        255}, ${a[1][1] * 255}, ${a[1][2] * 255})'> </div> <p>${
+                        a[0]
+                      }</p></div>`,
+                  )
+                  .join('\n')}
+            </div>
+                `;
+        keyDiv.innerHTML = keyString;
+      } else {
+        keyDiv.innerHTML = '';
+      }
+      console.log('Key string ', keyString);
+    };
+
+    let showColor = false;
+
+    const linkForce = d3.forceLink([]);
     const chargeForce = d3.forceManyBody().strength(-20);
     let simulation = d3
       .forceSimulation(nodes)
       .velocityDecay(0.2)
       .force('x', d3.forceX().strength(0.04))
       .force('y', d3.forceY().strength(0.04))
+      .force('link', linkForce)
       .force(
         'collide',
         d3
@@ -62,8 +143,6 @@ d3.csv(`${process.env.PUBLIC_URL}/dataset_stats.csv`).then(datasets => {
         .domain([min, max])
         .range(range);
 
-      window.scale = scale;
-
       nodes = nodes.map((n, i) => ({
         ...n,
         r: v ? scale(parseFloat(datasets[i][v])) : 10,
@@ -79,11 +158,25 @@ d3.csv(`${process.env.PUBLIC_URL}/dataset_stats.csv`).then(datasets => {
       depth: {enable: false},
       attributes: {
         position: (context, props) =>
-          props.nodes.map(n => [n.x * 0.001, n.y * 0.001]),
+          props.nodes.map(n => [n.x / props.width, n.y / props.height]),
         pointWidth: radiusBuffer,
-        nodeColor: (context, props) => props.nodes.map(e => e.color),
+        nodeColor: (context, props) =>
+          props.nodes.map(e => (showColor ? e.color : [0.8, 0.8, 0.8, 1.0])),
       },
       count: (context, props) => props.nodes.length,
+      primitive: 'points',
+    });
+
+    const drawPointer = regl({
+      vert: circleVert,
+      frag: circleFrag,
+      depth: {enable: false},
+      attributes: {
+        position: (context, props) => [[props.x, props.y]],
+        pointWidth: [50],
+        nodeColor: [[1.0, 0.0, 0.0, 1.0]],
+      },
+      count: 1,
       primitive: 'points',
     });
 
@@ -93,17 +186,24 @@ d3.csv(`${process.env.PUBLIC_URL}/dataset_stats.csv`).then(datasets => {
         color: [0, 0, 0, 1],
         depth: 1,
       });
-
-      drawParticles({nodes: [...simulation.nodes()]});
+      drawParticles({
+        nodes: [...simulation.nodes()],
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+      /*drawPointer({
+        x: wx,
+        y: wy,
+                });*/
     });
     document.addEventListener('keypress', e => {
       if (e.key === 'l') {
-        simulation.force('link', linkForce);
         simulation.force('charge', chargeForce);
+        simulation.force('link').links(links);
         simulation.alpha(0.4);
       }
       if (e.key === 'k') {
-        simulation.force('link', null);
+        simulation.force('link').links([]);
         simulation.force('charge', null);
         simulation.alpha(0.4);
       }
@@ -112,6 +212,10 @@ d3.csv(`${process.env.PUBLIC_URL}/dataset_stats.csv`).then(datasets => {
       }
       if (e.key === 'd') {
         setSizeVar('downloads');
+      }
+      if (e.key === 'c') {
+        showColor = !showColor;
+        setColorLegend();
       }
       if (e.key === 'j') {
         setSizeVar('d');
